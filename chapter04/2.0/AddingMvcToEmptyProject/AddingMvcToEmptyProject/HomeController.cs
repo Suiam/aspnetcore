81eae1a8a541dadafee725b97579cc7b4973a514
Suiam Costa <suiamcosta@hotmail.com>
2020-01-23 13:47:38 -0300

    aspnet core

---
+++ chapter04/2.0/AddingMvcToEmptyProject/AddingMvcToEmptyProject/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddingMvcToEmptyProject
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World!";
+        }
+    }
+}
